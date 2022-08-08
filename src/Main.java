727fb46cebdc94ced0d881962247a23fe13e38cd
Jerk <36136837+SwedenWarfare@users.noreply.github.com>
2022-08-09 01:11:54 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,24 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        if(args.length > 0)
+        {
+            try {
+                boolean idk = Boolean.parseBoolean(args[0]);
+                Gui.shouldShow = idk;
+            }catch (Exception e){}
+
+        }
         try {
-            //weird one javax.swing.plaf.nimbus.NimbusLookAndFeel
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-            //
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+                //weird one javax.swing.plaf.nimbus.NimbusLookAndFeel
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                //
+                //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 
 
-        } catch(Exception ignored){}
+            } catch(Exception ignored){}
         new createDirsAndFiles();
         new Gui();
+        }
     }
-}
